d0b9886980639d221bc9318ad3d90fee280e49d0
Alex Johnstone <alexjj@gmail.com>
2013-05-26 20:05:01 +0100

    trying to fix static stuff on heroku

--- dfworlds/urls.py
+++ dfworlds/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
@@ -10,4 +11,10 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='index'),
     url(r'^worlds/', include('worlds.urls', namespace="worlds")),
     url(r'^admin/', include(admin.site.urls), name='admin'),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )
